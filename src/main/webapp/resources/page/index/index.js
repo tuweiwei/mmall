edc87e5d7596916661b95063c8cf0fe2ed428b43
tuweiwei <2752624360@qq.com>
2017-08-02 14:21:23 +0800

    d

--- src/main/webapp/resources/page/index/index.js
+++ src/main/webapp/resources/page/index/index.js
@@ -1,3 +1,5 @@
 /**
  * Created by Administrator on 2017/8/1.
  */
+require('page/common/nav/index.js');
+var __m = require('util/tools.js');
--- src/main/webapp/resources/webpack.config.js
+++ src/main/webapp/resources/webpack.config.js
@@ -1,9 +1,28 @@
+var webpack  = require('webpack');
+var commonPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
+
 var config= {
     entry: './scripts/app.js',
     output: {
         path: './dist',
         filename: 'app.js'
+    },
+    plugins: [],
+    module: {
+        loaders: [
+            {},
+            {}
+        ]
+    },
+
+    resolve: {
+
+        alias: {
+
+        }
+
     }
+
 };
 
 module.exports  = config;
