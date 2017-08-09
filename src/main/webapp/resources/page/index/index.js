181ae49d7677dfa1813d421974c3224f54aecfd3
tuweiwei <2752624360@qq.com>
2017-08-09 14:16:52 +0800

    sdf

--- src/main/webapp/resources/page/index/index.js
+++ src/main/webapp/resources/page/index/index.js
@@ -7,6 +7,17 @@ var navside = require('page/common/nav-side/index.js');
 var __m = require('util/tools.js');
 require('./index.css');
 
-navside.init({
-    name : 'modify-password',
+$(function() {
+    // 渲染banner的html
+    var bannerHtml  = _mm.renderHtml(templateBanner);
+    $('.banner-con').html(bannerHtml);
+    // 初始化banner
+    var $slider     = $('.banner').unslider({
+        dots: true
+    });
+    // 前一张和后一张操作的事件绑定
+    $('.banner-con .banner-arrow').click(function(){
+        var forward = $(this).hasClass('prev') ? 'prev' : 'next';
+        $slider.data('unslider')[forward]();
+    });
 });
